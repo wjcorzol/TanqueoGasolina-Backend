9f82eb858e17b5061a76bd6fd5e76264731cf774
Willy Corzo <wjcorzol@gmail.com>
2021-12-16 20:55:44 -0500

    configuración express

---
+++ database/index.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const config = require('../config');
+
+const dbString = config.dbString;
+
+const init = async () => {
+    const options = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    };
+    try {
+    await mongoose.connect(dbString, options);
+    console.log('Connected to database successfully');
+    } catch (error) {
+        console.log(`Error message: ${error.message}`)};
+}
+
+module.exports = {init};
