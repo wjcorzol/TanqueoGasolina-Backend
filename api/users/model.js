8b86d006f7a592300c965f1ee895ba8e961933b4
FabianAbella <fabian.ing95@gmail.com>
2021-12-18 00:20:30 -0500

    cambio del model

--- api/users/model.js
+++ api/users/model.js
@@ -1,17 +1,48 @@
 const mongoose = require('mongoose');
 
 const collection = 'users';
+const collection = 'vehiculos';
+const collection = 'taqueos';
+const collection = 'ingresos';
 
 const userSchema = {
-    name: {type:String, required:[true, 'Name is required']},
-    documentId: {type:String, required:[true, 'DocumentId is required']},
-    noDocument: {type:String, required:[true, 'No. Document is required'], unique:true},
-    birthday: {type:DateView, required:[true, 'Birthday is required']},
-    sex: {type:String},
-    city: {type:String},
-    username: {type:String, required:[true, 'Username is required'], unique:true},
-    password:  {type:String, required:[true, 'Password is required']},
+    nombre: {type:String, required:[true, 'El nombre es obligatorio']},
+    tipoDocumento: {type:Boolean, required:[true, 'El tipo de documento es obligatorio']},
+    numeroDocumento: {type:String, required:[true, 'El número de documento es obligatorio'], unique:true},
+    nacimiento: {type:DateView, required:[true, 'La fecha de nacimiento es obligatoria']},
+    sexo: {type:Boolean},
+    ciudad: {type:String},
+    nombreUsuario: {type:String, required:[true, 'El nombre de usuario es obligatorio'], unique:true},
+    contraseña:  {type:String, required:[true, 'La contraseña es obligatoria']},
+    rol:  {type:Boolean},
+    saldoEfectivo:  {type:Float32Array},
+    saldoPuntos:  {type:Float32Array}
+}
 
+const vehiculoSchema = {
+    nombreUsuario: {type:String, required:[true, 'El nombre de usuario es obligatorio'], unique:true},
+    tipoVehiculo: {type:Boolean, required:[true, 'El tipo de vehículo es obligatorio']},
+    marcaVehiculo: {type:String, required:[true, 'La marca del vehículo es obligatorio']},
+    placaVehiculo: {type:String, required:[true, 'La placa del vehiculo es obligatoria']},
+    estado: {type:String, Boolean}
+}
+
+const tanquearSchema = {
+    placaVehiculo: {type:String, required:[true, 'La placa del vehiculo es obligatoria']},
+    tipoCombustible: {type:Boolean, required:[true, 'El tipo de combustible es obligatorio']},
+    galones: {type:Float32Array, required:[true, 'El número de galones es obligatorio']},
+    medioDePago: {type:Boolean, required:[true, 'El medio de pago es obligatorio']},
+    precioGalon: {type:Float32Array, required:[true, 'El precio del galon es obligatorio']},
+    valorTanqueo: {type:Float32Array, required:[true, 'El valor del tanqueo es obligatorio']}
+}
+
+const ingresarLoteSchema = {
+    nombreUsuario: {type:String, required:[true, 'El nombre de usuario es obligatorio'], unique:true},
+    tipoCombustible: {type:Boolean, required:[true, 'El tipo de combustible es obligatorio']},
+    galones: {type:Float32Array, required:[true, 'El número de galones es obligatorio']},
+    fechaIngreso: {type:DateView, required:[true, 'La fecha de ingreso es obligatoria']},
+    costo: {type:Float32Array, required:[true, 'El costo es obligatorio']},
+    fechaTerminacion: {type:DateView, required:[true, 'La fecha de terminación es obligatoria']}
 }
 
 const options = {
