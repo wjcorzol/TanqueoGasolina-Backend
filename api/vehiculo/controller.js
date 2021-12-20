2f83cac37dd755d85c7c5e6614222d6018c17b82
FabianAbella <fabian.ing95@gmail.com>
2021-12-20 13:19:08 -0500

    Controlador de Vehiculo

---
+++ api/vehiculo/controller.js
@@ -0,0 +1,23 @@
+const vehiculo = require('./model')
+
+const crearVehiculo = async (req, res) => {
+    const { placaVehiculo, tipoVehiculo, marcaVehiculo} = req.body;
+    const vehiculoEncontrado = await Vehiculo.find({placaVehiculo});
+
+    if(vehiculoEncontrado.length === 0){
+        const nuevoVehiculo = new Vehiculo({
+            placaVehiculo,
+            tipoVehiculo,
+            marcaVehiculo
+        });
+        nuevoVehiculo.save().then((vehiculoCreado) => {
+            res.status(200).json({vehiculoCreado});
+        });    
+    }else{
+        res.status(400).json({error:'El vehiculo ya existe'});
+    }
+}
+
+module.exports = {
+    crearVehiculo
+}
--- api/vehiculo/model.js
+++ api/vehiculo/model.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const collection = 'vehiculos';
 
 const vehiculoSchema = {
-    nombreUsuario: {type:String, required:[true, 'El nombre de usuario es obligatorio']},
+    nombreUsuario: {type: mongoose.Schema.Types.ObjectId, ref: "usuario"},
     tipoVehiculo: {type:Boolean, required:[true, 'El tipo de vehículo es obligatorio']},
     marcaVehiculo: {type:String, required:[true, 'La marca del vehículo es obligatorio']},
     placaVehiculo: {type:String, required:[true, 'La placa del vehiculo es obligatoria']},
-    estado: {type:String, Boolean}
+    estado: {type:Boolean, required: true, default: true}
 }
 
 const options = {
